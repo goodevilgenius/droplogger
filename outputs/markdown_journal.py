122a662b4a7ffc7a83b99ee5ef01333dd77a3916
Dan Jones <good.evil.genius@gmail.com>
2014-03-17 15:03:18 -0400

    Suport sub-entries in Markdow Journal output

--- outputs/markdown_journal.py
+++ outputs/markdown_journal.py
@@ -9,29 +9,18 @@ config = {"path": os.path.join(os.path.expanduser('~'),'Dropbox/Journal'),
           "short_date":"%Y-%m-%d","long_date":"%x",
           "date_time":"%c"}
 
-def add_entries(entries):
+def add_entries_helper(entries, key, level, fo):
     if not entries: return
-    
-    import os
-    if not os.path.isdir(config["path"]): os.mkdir(config["path"])
+    if not key: return
+    if not level: return
 
-    date = entries[entries.keys()[0]][0]["date"]
-    f = os.path.join(config["path"], config["filename"].format(date.strftime(config["short_date"])))
-    fo = open(f, 'w')
+    subs = find_sub_keys(entries, key)
+    if not key in entries and len(subs) == 0: return
 
-    fo.write("# " + config["main_header"].format(date.strftime(config["long_date"])))
+    fo.write(('#'*level) + ' ' + key)
     fo.write("\n\n")
-
-    if "diary" in entries:
-        for e in entries["diary"]:
-            fo.write(e["text"])
-            fo.write("\n\n")
-
-    for t in entries:
-        if t == "diary": continue
-        fo.write('## ' + t)
-        fo.write("\n\n")
-        for e in entries[t]:
+    if key in entries:
+        for e in entries[key]:
             fo.write('* ')
             fo.write(e["date"].strftime(config["date_time"]))
             fo.write(" - ")
@@ -40,3 +29,52 @@ def add_entries(entries):
             if "url" in e and e["url"] is not None: fo.write('](' + e["url"] + ')')
             fo.write("\n")
         fo.write("\n")
+        del entries[key]
+
+    for sk in subs:
+        add_entries_helper(entries, sk, level+1, fo)
+
+def find_sub_keys(entries, key):
+    if not entries: return
+    if not key: return
+
+    r = []
+    for k in entries:
+        if k.startswith(key + "/"): r.append(k)
+    return r
+
+def add_entries(entries):
+    if not entries: return
+    
+    import os, copy
+    if not os.path.isdir(config["path"]): os.mkdir(config["path"])
+
+    c = copy.deepcopy(entries)    
+
+    date = c[c.keys()[0]][0]["date"]
+    f = os.path.join(config["path"], config["filename"].format(date.strftime(config["short_date"])))
+    fo = open(f, 'w')
+
+    fo.write("# " + config["main_header"].format(date.strftime(config["long_date"])))
+    fo.write("\n\n")
+
+    if "diary" in c:
+        for e in c["diary"]:
+            fo.write(e["text"])
+            fo.write("\n\n")
+        del c["diary"]
+    
+    for k in find_sub_keys(c, "diary"):
+        for e in c[k]:
+            fo.write(e["text"])
+            fo.write("\n\n")
+        del c[k]
+
+    for t in c.keys():
+        if not t in c: continue
+        if t.count('/') > 0:
+            i = t.index('/')
+            add_entries_helper(c, t[:i], 2, fo)
+        add_entries_helper(c, t, 2, fo)
+    
+    fo.close()
