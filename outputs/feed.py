3bec837c7bd0c4f53c43a256e6c50c25ed67a537
Dan Jones <dan.jones@servermonkey.com>
2015-12-07 16:40:10 -0600

    I need to work on my variable names

--- outputs/feed.py
+++ outputs/feed.py
@@ -8,7 +8,9 @@ config = {"path": os.path.join(os.path.expanduser('~'),'Dropbox','Feed'),
           "filename":"feed_{1}_{2}.{0}", "date":"%Y-%m-%d", 
           "date_time":"%c", "formats": ['rss'],
           "ext":{"rss":"xml","atom":"xml","json":"json","jsonp":"js"},
-          "jsonp_callback":"drop_feed","stdout":False}
+          "jsonp_callback":"drop_feed","stdout":False,
+          "feed_link":"https://github.com/goodevilgenius/droplogger/",
+          "feed_title":"DropLogger feed for {}"}
 
 def add_entries(entries):
     if not entries: return
@@ -17,6 +19,20 @@ def add_entries(entries):
     if not config["stdout"] and not os.path.isdir(config["path"]): os.mkdir(config["path"])
 
     import jinja2
+
+    entries_to_send = []
+    for log in entries:
+        these_entries = {}
+        these_entries["entries"] = entries[log]
+        these_entries["log"] = log
+        these_entries["title"] = config["feed_title"].format(log)
+        use_title = "feed_description" not in config or config["feed_description"] is None
+        these_entries["description"] = these_entries["title"] if use_title else config["feed_description"].format(log)
+        these_entries["link"] = config["feed_link"]
+
+        entries_to_send.append(these_entries)
+        
+        
     
     templates_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates')
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_path))
@@ -29,9 +45,8 @@ def add_entries(entries):
             pass
 
         if temp is None: continue
-        for log in entries:
-            print(log)
-            the_entries = entries[log]
-            print(temp.render({"entries":the_entries,"log": log}))
+        for to_send in entries_to_send:
+            print(to_send)
+            print(temp.render(to_send))
             
             
--- outputs/templates/rss.tpl
+++ outputs/templates/rss.tpl
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
-  <title>Drop-Feed: {{ log }}</title>
-  <link>http://github.com/goodevilgenius/droplogger</link>
-  <description>Here's a description</description>
+  <title>{{ title }}</title>
+  <link>{{ link }}</link>
+  <description>{{ description }}</description>
 
   {% for entry in entries %}
   <item>
