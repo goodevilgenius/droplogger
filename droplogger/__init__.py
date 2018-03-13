7da460001766b0c434e1d9c6da04d9e60d091a09
Dan Jones <djones109+c9@gmail.com>
2018-03-13 18:26:27 +0000

    📚 Add docstring to __init__ #100DaysOfCode R1D55

---
+++ droplogger/__init__.py
@@ -0,0 +1,9 @@
+"""
+Copyright (2016-2018) Dan Jones <danjones@goodevilgenius.org>
+
+Logging application and python module.
+"""
+
+__author__ = "Dan Jones <danjones@goodevilgenius.org>"
+__license__ = "MIT"
+__version__ = '0.8.1'
