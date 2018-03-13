45ac3db519ce65f0882e52e28fb1936a7133e615
Dan Jones <djones109+c9@gmail.com>
2018-03-13 17:39:48 +0000

    🔖 Add version to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(name='droplogger',
       description='Logging application',
       url='https://github.com/goodevilgenius/droplogger',
+      version='0.8.1',
       author='Dan Jones',
       author_email='danjones@goodevilgenius.org',
       license='MIT',
