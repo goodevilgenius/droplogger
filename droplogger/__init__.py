8fb7d9546f1c6dd9d941a39147316a8d4be5d07e
Dan Jones <djones109+c9@gmail.com>
2018-03-14 16:46:08 +0000

    📚 Fill out `__init__`s

--- droplogger/__init__.py
+++ droplogger/__init__.py
@@ -1,7 +1,11 @@
-"""
-Copyright (2016-2018) Dan Jones <danjones@goodevilgenius.org>
+"""Copyright (2016-2018) Dan Jones <danjones@goodevilgenius.org>
 
 Logging application and python module.
+
+If using as an application, you'll want to use the `droplogger` and `drop-a-log` commands.
+
+If using as a module, you'll likely need droplogger.drop_a_log, and droplogger.config.
+The droplogger.droplogger module is currently too tied to the command-line to be useful otherwise.
 """
 
 __author__ = "Dan Jones <danjones@goodevilgenius.org>"
---
+++ droplogger/utils/__init__.py
@@ -0,0 +1,15 @@
+"""Copyright (2016-2018) Dan Jones <danjones@goodevilgenius.org>
+
+Logging application and python module.
+
+If using as an application, you'll want to use the `droplogger` and `drop-a-log` commands.
+
+If using as a module, you'll likely need droplogger.drop_a_log, and droplogger.config.
+The droplogger.droplogger module is currently too tied to the command-line to be useful otherwise.
+
+This module contains various utilities for the application.
+"""
+
+__author__ = "Dan Jones <danjones@goodevilgenius.org>"
+__license__ = "MIT"
+__version__ = '0.8.1'
