4084fd1861bbd2492e41aea3e7795a92a01e58a3
Dan Jones <djones109+c9@gmail.com>
2018-05-01 17:51:36 +0000

    Have stdout use serialize_json

--- droplogger/outputs/stdout.py
+++ droplogger/outputs/stdout.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python
 
 import json
+from ..utils.misc import serialize_json
 __all__ = ["add_entries"]
 config = {"__Instructions__":"if json_output is true, will be output in JSON format. With indent set true, it will be pretty-printed","json_output": False, "indent": True}
-
-class MyJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        try:
-            e = json.JSONEncoder()
-            e.encode(obj)
-        except TypeError:
-            return (str)(obj)
-        return json.JSONEncoder.default(self, obj)
 
 def add_entries(entries):
     if config['json_output']:
         import copy
         c = copy.deepcopy(entries)
-        if config['indent']: print(json.dumps(c, indent=4, cls=MyJSONEncoder))
-        else: print(json.dumps(c, cls=MyJSONEncoder))
+        if config['indent']: print(json.dumps(c, indent=4, default=serialize_json))
+        else: print(json.dumps(c, default=serialize_json))
     else:
         import os
         for cat,ents in entries.items():
